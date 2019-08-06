fa3705a55766824e2cc6bf920a262f86fe5d7e80
Tristan Grovender <therealtristangrovender@gmail.com>
2019-08-06 17:30:00 -0500

    Everything done except calender and classes

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -9,6 +9,9 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Print all the contents of the file, then close the file
 
 # YOUR CODE HERE
+file = open("foo.txt", "r")
+print(file.read())
+file.close()
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
@@ -16,3 +19,10 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+new_file = open("bar.txt", "w")
+
+new_file.write("No matter what people tell you, ")
+new_file.write("words and ideas can change the world")
+new_file.write(" - Robin Williams")
+
+new_file.close()
